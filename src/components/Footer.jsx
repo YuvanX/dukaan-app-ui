f3f5adf35869bd38b9ab40a420f68e3b67e98493
K. Abhi Vignesh <132771557+YuvanX@users.noreply.github.com>
2024-09-08 11:22:01 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
                    <p className="text-gray-500">---- Made By </p><a className="font-medium text-blue-700 ml-1" href="https://twitter.com/AbhiVignesh_/">Abhi Vignesh</a>, <p className="text-gray-500 ml-1">inspired by </p><a className="font-medium text-blue-700 ml-1">Dukaan App UI ----</a>
         </div>
         <div className="pt-2 pb-10">
-            <a href="https://github.com/YuvanX/payTM-v1" className="underline text-gray-500 ml-36">
+            <a href="https://github.com/YuvanX/dukaan-app-ui" className="underline text-gray-500 ml-36">
                 Get the source code
             </a>
         </div>
